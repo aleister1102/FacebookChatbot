a3ea66155c6fd9f03d53b1e053ffddffba325e2e
Lê Minh Quân <marucube35@gmail.com>
2022-08-27 16:04:56 +0700

    fix: missing Event model in event handler

--- src/handlers/event.handlers.js
+++ src/handlers/event.handlers.js
@@ -2,6 +2,8 @@ const callSendAPI = require('../utils/callSendAPI')
 
 const { templates } = require('../templates/')
 
+const Event = require('../models/event')
+
 const eventImage =
     'https://images.pexels.com/photos/8566472/pexels-photo-8566472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
 
