5338dbf9dc0d1887e978c6ca6e9c99437194dbbb
Lê Minh Quân <marucube35@gmail.com>
2022-08-27 19:36:31 +0700

    fix: AutoIncrement can only be used once

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,12 @@
 const mongoose = require('mongoose')
-const AutoIncrement = require('mongoose-sequence')(mongoose)
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema(
     {
-        // private fields
-        _id: Number,
-
-        // public fields
         psid: { type: String, unique: true },
         memes: Number,
     },
-    { _id: false },
+    { _id: false, timestamps: true },
 )
-
-UserSchema.plugin(AutoIncrement)
 
 module.exports = mongoose.model('User', UserSchema)
