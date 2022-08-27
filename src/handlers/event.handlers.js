8189661109fb3ddfa6c4d6f0c04a5de2d265e767
Lê Minh Quân <marucube35@gmail.com>
2022-08-27 16:06:57 +0700

    fix: wrong imported module name

--- src/handlers/event.handlers.js
+++ src/handlers/event.handlers.js
@@ -2,7 +2,7 @@ const callSendAPI = require('../utils/callSendAPI')
 
 const { templates } = require('../templates/')
 
-const Event = require('../models/event')
+const Event = require('../models/Event')
 
 const eventImage =
     'https://images.pexels.com/photos/8566472/pexels-photo-8566472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
