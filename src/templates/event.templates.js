06c6e5b50d1e0dd283dc118110d8e58d3b6bc0cd
Lê Minh Quân <marucube35@gmail.com>
2022-08-29 13:10:27 +0700

    fix: image in event list template

--- src/templates/event.templates.js
+++ src/templates/event.templates.js
@@ -8,11 +8,11 @@ function EventListTemplate(eventList) {
                     ...eventList,
                     {
                         title: `Trở về`,
+                        image_url:
+                            'https://images.pexels.com/photos/8566472/pexels-photo-8566472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                         buttons: [
                             {
                                 type: 'postback',
-                                image_url:
-                                    'https://images.pexels.com/photos/8566472/pexels-photo-8566472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                                 title: 'Quay về menu chính',
                                 payload: `MAIN_MENU`,
                             },
