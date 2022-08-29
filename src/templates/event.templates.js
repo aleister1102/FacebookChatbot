1337ff3ffd0a13719db1a3794bb5a7e36828f9a6
Lê Minh Quân <marucube35@gmail.com>
2022-08-29 13:07:21 +0700

    chore: add image to event list template

--- src/templates/event.templates.js
+++ src/templates/event.templates.js
@@ -11,6 +11,8 @@ function EventListTemplate(eventList) {
                         buttons: [
                             {
                                 type: 'postback',
+                                image_url:
+                                    'https://images.pexels.com/photos/8566472/pexels-photo-8566472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                                 title: 'Quay về menu chính',
                                 payload: `MAIN_MENU`,
                             },
